1e928329ff285444c0a1cab1d4d8ba3d66356451
Ethan Campbell <to239@cummins.com>
2024-07-15 11:33:49 -0400

    use default prettier config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
